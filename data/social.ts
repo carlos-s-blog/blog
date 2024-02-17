5e3f45f4b439571f01590cd7b7a29f9bf2da6b16
G1-777 <println_g1@163.com>
2024-02-17 20:42:00 +0800

    add myself

--- data/social.ts
+++ data/social.ts
@@ -1,13 +1,13 @@
 export type Social = {
   github?: string
-  twitter?: string
-  juejin?: string
+  // twitter?: string
+  // juejin?: string
   qq?: string
   wx?: string
-  cloudmusic?: string
-  zhihu?: string
+  // cloudmusic?: string
+  // zhihu?: string
   email?: string
-  discord?: string
+  // discord?: string
 }
 
 type SocialValue = {
@@ -18,42 +18,42 @@ type SocialValue = {
 }
 
 const social: Social = {
-  github: 'https://github.com/kuizuo',
-  twitter: 'https://twitter.com/kuizuo',
-  juejin: 'https://juejin.cn/user/1565318510545901',
-  qq: 'https://img.kuizuo.cn/qq.png',
-  wx: 'https://img.kuizuo.cn/wechat.png',
+  github: 'https://github.com/CarlosLees',
+  // twitter: 'https://twitter.com/kuizuo',
+  // juejin: 'https://juejin.cn/user/1565318510545901',
+  qq: 'https://img.carloslee.work/upload/IMG_3677.JPG',
+  wx: 'https://img.carloslee.work/upload/IMG_3678.JPG',
   // zhihu: 'https://www.zhihu.com/people/kuizuo',
   // cloudmusic: 'https://music.163.com/#/user/home?id=1333010742',
-  email: 'mailto:hi@kuizuo.cn',
-  discord: 'https://discord.gg/M8cVcjDxkz',
+  email: 'mailto:println_g1@163.com',
+  // discord: 'https://discord.gg/M8cVcjDxkz',
 }
 
-const socialSet: Record<keyof Social | 'rss', SocialValue> = {
+const socialSet: Record<keyof Social, SocialValue> = {
   github: {
     href: social.github,
     title: 'GitHub',
     icon: 'ri:github-line',
     color: '#010409',
   },
-  juejin: {
-    href: social.juejin,
-    title: '掘金',
-    icon: 'simple-icons:juejin',
-    color: '#1E81FF',
-  },
-  twitter: {
-    href: social.twitter,
-    title: 'Twitter',
-    icon: 'ri:twitter-line',
-    color: '#1da1f2',
-  },
-  discord: {
-    href: social.discord,
-    title: 'Discord',
-    icon: 'ri:discord-line',
-    color: '#5A65F6',
-  },
+  // juejin: {
+  //   href: social.juejin,
+  //   title: '掘金',
+  //   icon: 'simple-icons:juejin',
+  //   color: '#1E81FF',
+  // },
+  // twitter: {
+  //   href: social.twitter,
+  //   title: 'Twitter',
+  //   icon: 'ri:twitter-line',
+  //   color: '#1da1f2',
+  // },
+  // discord: {
+  //   href: social.discord,
+  //   title: 'Discord',
+  //   icon: 'ri:discord-line',
+  //   color: '#5A65F6',
+  // },
   qq: {
     href: social.qq,
     title: 'QQ',
@@ -66,30 +66,24 @@ const socialSet: Record<keyof Social | 'rss', SocialValue> = {
     icon: 'ri:wechat-2-line',
     color: '#07c160',
   },
-  zhihu: {
-    href: social.zhihu,
-    title: '知乎',
-    icon: 'ri:zhihu-line',
-    color: '#1772F6',
-  },
+  // zhihu: {
+  //   href: social.zhihu,
+  //   title: '知乎',
+  //   icon: 'ri:zhihu-line',
+  //   color: '#1772F6',
+  // },
   email: {
     href: social.email,
     title: '邮箱',
     icon: 'ri:mail-line',
     color: '#D44638',
   },
-  cloudmusic: {
-    href: social.cloudmusic,
-    title: '网易云',
-    icon: 'ri:netease-cloud-music-line',
-    color: '#C20C0C',
-  },
-  rss: {
-    href: '/blog/rss.xml',
-    title: 'RSS',
-    icon: 'ri:rss-line',
-    color: '#FFA501',
-  },
+  // cloudmusic: {
+  //   href: social.cloudmusic,
+  //   title: '网易云',
+  //   icon: 'ri:netease-cloud-music-line',
+  //   color: '#C20C0C',
+  // },
 }
 
 export default socialSet
